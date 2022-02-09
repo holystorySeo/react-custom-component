d3e60115e626e6e23f5ce8e0736e4b0910c9ddd7
holystorySeo <holystory.seo@gmail.com>
2022-02-09 11:38:18 +0900

    Add AutoComplete

---
+++ custom-component/src/component/AutoComplete.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  display: block;
+  border: 10px solid black;
+  text-align : center;
+  width: 30%;
+`;
+
+export const AutoComplete = () => {
+  return (
+    <>
+      {/* <ModalContainer /> */}
+    </>
+  )
+}
+
