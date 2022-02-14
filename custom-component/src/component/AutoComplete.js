0e333fe845951d412c0e2f48008ff1ae08a9d9aa
holystorySeo <holystory.seo@gmail.com>
2022-02-15 02:15:40 +0900

    Fix DropDownList in AutoComplete

--- custom-component/src/component/AutoComplete.js
+++ custom-component/src/component/AutoComplete.js
@@ -1,22 +1,24 @@
-import styled from 'styled-components';
+import { useState } from 'react/cjs/react.development';
+import styled from 'styled-components'; 
 
 export const AutoCompleteWrapperr = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  margin: 0 auto;  
   height: 87%;
   width: 100%;
+  border: 1px solid black;
 `;
 
 export const AutoCompleteContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 7rem;
   width: 80%;
   height: 15%;
   border: 2px solid #E6E6E6;
-  border-radius: 30px;
+  border-radius: ${(props) => (props.hasText ? '1rem 1rem 0 0' : '1rem')};
 `;
 
 export const AutoCompleteInput = styled.input`
@@ -24,6 +26,7 @@ export const AutoCompleteInput = styled.input`
   height: 70%;
   border: none;
   font-size: 15px;
+  margin-left: 10px;
   margin-right: 3px;
 
   :focus {
@@ -37,17 +40,91 @@ export const AutoCompleteCloseIcon = styled.span`
   align-items: center;
   width: 15px;
   height: 15px;
+  margin-right: 5px;
   cursor: pointer;
 `;
 
+export const DropDownContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  min-height: 15%;
+  border-top: none;
+  border-right: 2px solid #E6E6E6;
+  border-bottom: 2px solid #E6E6E6;
+  border-left: 2px solid #E6E6E6;
+  border-radius: 0 0 1rem 1rem;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+
+  >.dropDownList {
+    width: 98%;
+    padding-left: 15px;
+    font-size: 15px;
+    margin-top: 5px;
+
+    &:hover {
+      background: #E6E6E6;
+  }
+  }
+`;
+
 export const AutoComplete = () => {
+  const dataBase = [
+    'culture', 'experience', 'education', 
+    'symbol', 'effect', 'liberty', 
+    'affair', 'comfort', 'affair'
+  ]
+  const [ inputValue, setInputValue ] = useState('')
+  const [ hasText, setHasText ] = useState(false);
+  const [ matchedList, setMatchedList ] = useState([])
+
+  const handleCloseDropDown = () => {
+    setInputValue('');
+    setHasText(false);
+  }
+
+  const checkInputValue = (e) => {
+    setInputValue(e.target.value);
+    setHasText(true);
+    setMatchedList(dataBase.filter((el) => {
+      return el.includes(e.target.value);
+    }))
+
+    if(e.target.value === '') {
+      setHasText(false);
+    }
+  }
+
+  const removeInputValue = () => {
+    setInputValue('');
+    setHasText(false);
+  }
+
+  const selectList = (e) => {
+    e.stopPropagation();
+    setInputValue(e.target.textContent)
+  }
+
   return (
     <>
-      <AutoCompleteWrapperr>
-        <AutoCompleteContainer>
-          <AutoCompleteInput />
-          <AutoCompleteCloseIcon>&times;</AutoCompleteCloseIcon>
+      <AutoCompleteWrapperr onClick={handleCloseDropDown}>
+        <AutoCompleteContainer hasText={hasText}>
+          <AutoCompleteInput value={inputValue} onKeyPress={checkInputValue} onChange={checkInputValue}></AutoCompleteInput>
+          <AutoCompleteCloseIcon onClick={removeInputValue}>&times;</AutoCompleteCloseIcon>
         </AutoCompleteContainer>
+        {hasText 
+          ? <DropDownContainer>
+              {matchedList.map((list, idx) => {
+                return (
+                  <li key={idx} className='dropDownList' onClick={selectList}>{list}</li>
+                )
+              })}
+            </DropDownContainer> 
+          : ''}
       </AutoCompleteWrapperr>
     </>
   )
