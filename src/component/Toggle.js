ae64ae280dd058cd3744a6f74867f2900d630b9e
holystorySeo <holystory.seo@gmail.com>
2022-02-18 00:20:17 +0900

    Toggle 컴포넌트 반응형 구현 브라우저 사이즈 줄이면 toggle 안의 circle이 container 벗어나는 문제 해결을 위해 circle을 container의 자식태그로 넣어주었다.

--- src/component/Toggle.js
+++ src/component/Toggle.js
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import styled from 'styled-components';
 
 export const ToggleContainer = styled.div`
-  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin-top: 8rem;
-  left: 47%;
 
   >.toggle-container {
     position: relative;
@@ -12,46 +13,30 @@ export const ToggleContainer = styled.div`
     height: 24px;
     border-radius: 30px;
     background-color: #8b8b8b;
-    transition: all ease .2s 0s;
-    cursor: pointer;
-  }
-
-  >.toggle-container2 {
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 0px;
-    height: 24px;
-    border-radius: 30px;
     transition: all ease .3s 0s;
-
+    cursor: pointer;
     &.toggle--checked {
-      width: 60px;
-      transition: all ease .3s 0s;
       background-color: #8000FF;
     }
-  }
 
-  >.toggle-circle {
-    position: absolute;
-    top: 3px;
-    left: 4px;
-    width: 18px;
-    height: 18px;
-    border-radius: 50%;
-    background-color: #ffffff;
-    transition: all ease .3s 0s;
-    cursor: pointer;
-
-    &.toggle--checked {
+    >.toggle-circle {
+      position: absolute;
+      top: 3px;
+      left: 5px;
+      width: 18px;
+      height: 18px;
+      border-radius: 50%;
+      background-color: #ffffff;
       transition: all ease .3s 0s;
-      left: 38px;
+      cursor: pointer;
+      &.toggle--checked {
+        left: 37px;
+      }
     }
   }
 
   >.desc {
     margin-top: 10px;
-    margin-left: -40px;
   }
 `;
 
@@ -65,9 +50,9 @@ export const Toggle = () => {
   return (
     <>
       <ToggleContainer>
-        <div className='toggle-container' onClick={toggleHandler} />
-        <div className={`toggle-container2 ${isToggleOn ? 'toggle--checked' : ''}`} onClick={toggleHandler} />
-        <div className={`toggle-circle ${isToggleOn ? 'toggle--checked' : ''}`}onClick={toggleHandler} />
+        <div className={`toggle-container ${isToggleOn ? 'toggle--checked' : ''}`} onClick={toggleHandler}>
+          <div className={`toggle-circle ${isToggleOn ? 'toggle--checked' : ''}`} onClick={toggleHandler} />
+        </div>
         <div className='desc' onClick={toggleHandler}>{isToggleOn ? 'Toggle Switch ON' : 'Toggle Switch OFF'}</div> 
       </ToggleContainer>
     </>
