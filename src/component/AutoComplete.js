f54fd3ae9382997101ac7e70e73b7f41fa42487b
holystorySeo <holystory.seo@gmail.com>
2022-02-22 16:01:14 +0900

    AutoComplete 드롭다운 방향키 키능 구현

--- src/component/AutoComplete.js
+++ src/component/AutoComplete.js
@@ -118,16 +118,34 @@ export const AutoComplete = () => {
   }
 
   const handleKeyPress = (e) => {
-    const maxId = matchedList.length;
+    const maxId = matchedList.length; // 드롭다운 리스트의 최대 인덱스값을 구한다.
 
     if(e.key === 'ArrowDown') {
       if(isIdxSelected === undefined) { //맨처음 키보드 down 할 때 드랍다운 리스트 인덱스 0번을 선택한다.
         setIsIdxSelected(0);
+        setInputValue(matchedList[0]);
       } else if(isIdxSelected !== undefined) { // 두번째 키보드 down 부터
         if(maxId === isIdxSelected + 1) { // maxId와 isIdxSelected 값의 차이가 1일 경우 인덱스는 0을 설정한다.
           setIsIdxSelected(0); // isIdxSelected의 값은 0이 된다.
+          setInputValue(matchedList[0]);
         } else { // 나머지의 경우에는 idx를 1씩 증가시킨다.
           setIsIdxSelected(isIdxSelected +  1);
+          setInputValue(matchedList[isIdxSelected +  1]);
+        }
+      }
+    }
+
+    if(e.key === 'ArrowUp') {
+      if(isIdxSelected === undefined) {
+        setIsIdxSelected(maxId - 1);
+        setInputValue(matchedList[maxId - 1]);
+      } else if(isIdxSelected !== undefined) {
+        if(isIdxSelected === 0) {
+          setIsIdxSelected(maxId - 1);
+          setInputValue(matchedList[maxId - 1]);
+        } else {
+          setIsIdxSelected(isIdxSelected - 1)
+          setInputValue(matchedList[isIdxSelected - 1]);
         }
       }
     }
