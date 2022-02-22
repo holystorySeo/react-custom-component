adc5972989fb6d7fbc54b89a883f1422f5708641
holystorySeo <holystory.seo@gmail.com>
2022-02-22 15:43:51 +0900

    방향키 다운이 드롭다운 목록의 끝에 올 경우 처리 완료 드랍다운 마지막 인덱스에서 down 방향키 누를 경우 드랍다운 목록의 맨처음으로 이동 기능 구현

--- src/component/AutoComplete.js
+++ src/component/AutoComplete.js
@@ -118,19 +118,17 @@ export const AutoComplete = () => {
   }
 
   const handleKeyPress = (e) => {
+    const maxId = matchedList.length;
+
     if(e.key === 'ArrowDown') {
-      if(isIdxSelected === undefined) {
+      if(isIdxSelected === undefined) { //맨처음 키보드 down 할 때 드랍다운 리스트 인덱스 0번을 선택한다.
         setIsIdxSelected(0);
-        setInputValue(matchedList[0]);
-      } else {
-        setIsIdxSelected((isIdxSelected + 1))
-        setInputValue(matchedList[isIdxSelected + 1]);
-      }
-    } else if(e.key === 'ArrowUp') {
-      if(isIdxSelected === undefined) {
-      } else {
-        setIsIdxSelected((isIdxSelected - 1))
-        setInputValue(matchedList[isIdxSelected - 1]);
+      } else if(isIdxSelected !== undefined) { // 두번째 키보드 down 부터
+        if(maxId === isIdxSelected + 1) { // maxId와 isIdxSelected 값의 차이가 1일 경우 인덱스는 0을 설정한다.
+          setIsIdxSelected(0); // isIdxSelected의 값은 0이 된다.
+        } else { // 나머지의 경우에는 idx를 1씩 증가시킨다.
+          setIsIdxSelected(isIdxSelected +  1);
+        }
       }
     }
   }
